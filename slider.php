1da5d98235fdc63545f0b8c111e82cb2034a6365
Alexander Block <alex@yazamo.com>
2013-06-26 09:35:09 -0400

    Modified bjqs.css to prevent list bullets from appearing.

--- slider.php
+++ slider.php
@@ -26,18 +26,22 @@ License: GPLv2
 
 function AddBJQSScripts(){
 wp_register_script('bjqs-js', plugins_url('/js/bjqs-1.3.min.js',__FILE__));
+wp_register_style('bjqs-demo-stylesheet', plugins_url('/css/demo.css',__FILE__));
 wp_register_style('bjqs-stylesheet', plugins_url('/css/bjqs.css',__FILE__));
 wp_enqueue_script('jquery');
+wp_enqueue_script('jquery-ui');
+wp_enqueue_script('jquery-ui-tabs');
 wp_enqueue_script('bjqs-js');
+wp_enqueue_style('bjqs-demo-stylesheet');
 wp_enqueue_style('bjqs-stylesheet');
 }
 add_action( 'wp_enqueue_scripts', 'AddBJQSScripts' );
 
 function SliderJS(){
 echo "<script class='secret-source'>
-var j = jQuery.noConflict();
-j(document).ready(function($) {
-          j('#banner-fade').bjqs({
+var r = jQuery.noConflict();
+r(document).ready(function($) {
+          r('#banner-fade').bjqs({
             height      : 320,
             width       : 620,
             responsive  : true
@@ -58,4 +62,23 @@ function register_basicslider_post_type() {
 		'supports'      => array( 'title', 'editor', 'thumbnail')
 	));
 }
+
+function OutputBasicSlider(){
+SliderJS();
+echo ' <!--  Outer wrapper for presentation only, this can be anything you like -->
+      <div id="banner-fade">
+
+        <!-- start Basic Jquery Slider -->
+        <ul class="bjqs">
+          <li><img src="http://placehold.it/350x150" title="Automatically generated caption"></li>
+          <li><img src="http://placehold.it/350x151" title="Automatically generated caption"></li>
+          <li><img src="http://placehold.it/350x152" title="Automatically generated caption"></li>
+        </ul>
+        <!-- end Basic jQuery Slider -->
+
+      </div>
+      <!-- End outer wrapper -->
+';
+}
+add_shortcode('BasicSlider','OutputBasicSlider');
 ?>
